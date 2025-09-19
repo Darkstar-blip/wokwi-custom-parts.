15cf4afa0dffabc1363e99eb8f88e0417175272a
Darkstar-blip <dwivediaakash37@gmail.com>
2025-09-19 12:29:31 +0530

    Create bldc.js

---
+++ bldc/bldc.js
@@ -0,0 +1,8 @@
+// BLDC motor custom part logic (stub for Wokwi simulation)
+class BLDC {
+  constructor(part) {
+    this.part = part;
+  }
+  // Add simulation logic as needed
+}
+module.exports = BLDC;
