bd1444ab9dda825eb26ef663a6d4199dd55c6949
Darkstar-blip <dwivediaakash37@gmail.com>
2025-09-19 12:27:20 +0530

    Create nrf24L10.js

---
+++ nrf24l01/nrf24L10.js
@@ -0,0 +1,8 @@
+// NRF24L10 custom part logic (stub for Wokwi simulation)
+class NRF24L10 {
+  constructor(part) {
+    this.part = part;
+  }
+  // Add simulation logic as needed
+}
+module.exports = NRF24L10;
